3bdd34602eaf9a8d269a7ac73e318ba15b491a78
Gerrard Elibe <gerardelibe@gmail.com>
2025-02-26 11:49:26 -1100

    updated

--- src/Auth/Student_signup/Student_Signup.jsx
+++ src/Auth/Student_signup/Student_Signup.jsx
@@ -40,7 +40,7 @@ const Student_Signup = () => {
   return (
     <div className="signup-container-6">
       <div className="signup-box-6">
-        <h2 style={{ color: "black", fontSize: "40px", textAlign: "center", paddingBottom: "25px" }}>
+        <h2 style={{ color: "black", fontSize: "50px",display:"flex", textAlign: "center", gap:"80px", justifyContent:"center", paddingBottom: "25px" }}>
           Student Application
         </h2>
         <form className="signup-form-6">
