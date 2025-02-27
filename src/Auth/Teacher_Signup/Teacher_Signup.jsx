35b402ab256cf0091813313e0cda7ed0a3522992
Benjamin <benjaminufuomajacob9@gmail.com>
2025-02-27 20:29:53 +0100

    my final commit message

--- src/Auth/Teacher_Signup/Teacher_Signup.jsx
+++ src/Auth/Teacher_Signup/Teacher_Signup.jsx
@@ -1,51 +1,69 @@
-import React from 'react'
-import '../../Auth/Teacher_Signup/Teacher_Signup.css'
+import React from "react";
+import "../../Auth/Teacher_Signup/Teacher_Signup.css";
 import { HiArrowLeft } from "react-icons/hi";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Teacher_Signup = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <>
-    <div className="theHeaderPusher"></div>
-    <div className='teachersignwrap'>
-      <div className='teachersignupname'>
-        <HiArrowLeft onClick={()=>navigate('/Get_Involved')}/>
-      <h1>Teaching Assistant Application</h1>
-      </div>
-      <div className='teachersignupforms'>
-        <div className='firstndlast'>
-          <div className="inputNames">
-          <label >Full Name</label>
-          
+      <div className="teachersignwrap">
+        <div className="teachersignupname">
+          <HiArrowLeft
+            size={30}
+            style={{ color: "#048c63" }}
+            onClick={() => navigate("/Get_Involved")}
+          />
+          <h1>Teaching Assistant Application</h1>
+        </div>
+        <div className="teachersignupforms">
+          <div className="firstndlast">
+            <div className="inputNames">
+              <label>Full Name</label>
+            </div>
+            <input type="text" placeholder="First name" className="namesbtn" />
+            <input type="text" placeholder="Last name" className="namesbtn" />
           </div>
-          <input type="text" placeholder='First name' className='namesbtn' />
-          <input type="text" placeholder='Last name' className='namesbtn' />
-        </div>
 
-        <div className='email'>
-          <label>Email</label>
-          <input type="email" placeholder='example@email.com' className='emailsbtn' />
-        </div>
+          <div className="email">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="example@email.com"
+              className="emailsbtn"
+            />
+          </div>
 
-        <div className='email'>
-          <label >Phone Number</label>
-          <input type="number" placeholder='080xxxxxxx' className='emailsbtn' />
-        </div>
+          <div className="email">
+            <label>Phone Number</label>
+            <input
+              type="number"
+              placeholder="080xxxxxxx"
+              className="emailsbtn"
+            />
+          </div>
 
-        <div className='email'>
-          <label >Address</label>
-          <input type="text" placeholder='1234 Street name,City,State' className='emailsbtn' />
-        </div>
+          <div className="email">
+            <label>Address</label>
+            <input
+              type="text"
+              placeholder="1234 Street name,City,State"
+              className="emailsbtn"
+            />
+          </div>
 
-        <div className='email'>
-          <label >GitHub Link(If available)</label>
-          <input type="url" placeholder='https://github.com/username/repository' className='emailsbtn' />
+          <div className="email">
+            <label>GitHub Link(If available)</label>
+            <input
+              type="url"
+              placeholder="https://github.com/username/repository"
+              className="emailsbtn"
+            />
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Teacher_Signup
+export default Teacher_Signup;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,109 +1,127 @@
-import React from 'react'
+import React from "react";
 import { IoIosArrowRoundForward } from "react-icons/io";
-import './home.css'
+import "./home.css";
 
 const Home = () => {
   return (
-    <div className='homebody'>
+    <div className="homebody">
       {/* section1 */}
-     <div className='sec1'>
-       <h1><b>Learn to code, Build Your Future.</b></h1>
-       <p>NaijaCoder organizes a free, intensive program in Nigeria to teach the basics of algorithms and Computer programming to high schoolers. The program also provide resources for students applying to universities abroad.<br/> Programming offers these students, especially those from indigent backgrounds, an opportunity to learn profitable skills and ignite their passions for problem-solving and critical thinking.</p>
-      <div className='sec1button'>
-       <button style={{
-        padding: '10px 30px',
-        margin: '10px',
-        borderRadius: '37px',
-        border: '4px solid #048c63',
-        color: 'green',
-        textDecoration: 'none',
-       }}>Learn more</button>
+      <div className="sec1">
+        <h1>Learn to code, Build Your Future.</h1>
+        <p>
+          NaijaCoder organizes a free, intensive program in Nigeria to teach the
+          basics of algorithms and Computer programming to high schoolers. The
+          program also provide resources for students applying to universities
+          abroad.
+          <br /> Programming offers these students, especially those from
+          indigent backgrounds, an opportunity to learn profitable skills and
+          ignite their passions for problem-solving and critical thinking.
+        </p>
+        <div className="sec1button">
+          <button
+            className="learnmore"
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <span className="span">Learn more</span>
+          </button>
 
-       <button style={{
-        padding: '10px 30px',
-        margin: '10px',
-        borderRadius: '37px',
-         backgroundColor: '#048c63',
-        color:'white',
-        textDecoration: 'none'
-       }}>Apply Now</button>
-       </div>
-     </div>
+          <button
+            className="applynow"
+            style={{
+              textDecoration: "none",
+            }}
+          >
+            <span className="span1">Apply Now</span>
+          </button>
+        </div>
+      </div>
 
+      {/* section2 */}
+      <div className="sec2">
+        <img style={{ width: "180px" }} src="\Group 140.png" />
 
-     {/* section2 */}
-     <div className='sec2'>
-        <img src='\Group 140.png' />
-
-        <div className='sec2img'>
-          <img src='\pexels-ron-lach-9783812.jpg' />
-          <p>providing free instructions on basics of algorithms</p>
-       </div>
-
-       <div className='getstarted'>
-        <p style={{
-          color:'white',
-          fontSize:'20px'
-        }}>Invest in tomorrow's innovators-support our youth coding program today</p>
-       <button style={{
-        fontSize: '37px',
-        color:'white',
-        backgroundColor:'#048c63',
-        margin:'20px',
-        textDecoration:'none'
-       }}>Get Started</button>
-       </div>
-
-        <div className='sec2img'>
-          <img src='\pexels-tonyzebastian-10011662 (1).jpg' />
+        <div className="sec2img">
+          <img src="\pexels-ron-lach-9783812.jpg" />
           <p>providing free instructions on basics of algorithms</p>
         </div>
 
-          <img src='\Group 141.png' />
-     </div>
+        <div className="getstarted">
+          <p
+            style={{
+              color: "white",
+              fontSize: "18px",
 
-     <div className='sec3'>
+              height: "160px",
+              display: "flex",
+              justifyContent: "flexend",
+              alignItems: "center",
+            }}
+          >
+            Invest in tomorrow's innovators-support our youth coding program
+            today
+          </p>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100px",
+            }}
+          >
+            <button
+              className="getstartedbutton"
+              style={{
+                textDecoration: "none",
+              }}
+            >
+              <span className="getstartedspan">Get Started</span>
+            </button>
+          </div>
+        </div>
 
-      <h2><b>Our Supporters</b></h2>
-      <div className='supporters'>
-       <img style={{
-         width: '140px',
-         height: '140px'
-       }} src='\mastercard.jpeg'  alt='mastercard'/>
-       <img  style={{
-         width: '260px',
-         height: '140px'
-       }} src='\simonsFoundation.png' alt='SimonsFoundation'/>
-       <img style={{
-         width: '240px',
-         height: '200px'
-       }} src='\Berkeley.png' alt='Berkeley' />
-       <img style={{
-         width: '370px',
-         height: '140px'
-       }} src='/mercatus.png' alt='mercatus'/>
-      
+        <div className="sec2img">
+          <img src="\pexels-tonyzebastian-10011662 (1).jpg" />
+          <p>providing free instructions on basics of algorithms</p>
+        </div>
+
+        <img style={{ width: "180px" }} src="/Group 141.png" />
       </div>
-      
-     </div>
 
-     <div className='sec4'>
-      <h3>Our Gallery</h3>
-
-      <div className='gallery'>
-      <img src='\WhatsApp Image 2025-02-25 at 14.38.15_0e404587.jpg' />
-      <div className='imgBody'>
-      <img src='\WhatsApp Image 2025-02-25 at 14.52.29_cfa199ef.jpg' />
-      <img src='\WhatsApp Image 2025-02-25 at 14.41.53_832e0478.jpg' />
+      <div className="sec3">
+        <h2>Our Supporters</h2>
+        <div className="supporters">
+          <img className="querryimg1" src="/mastercard.jpeg" alt="mastercard" />
+          <img
+            className="querryimg2"
+            src="\simonsFoundation.png"
+            alt="SimonsFoundation"
+          />
+          <img className="querryimg3" src="/Berkeley.png" alt="Berkeley" />
+          <img className="querryimg4" src="/mercatus.png" alt="mercatus" />
+        </div>
       </div>
-       <img src='\WhatsApp Image 2025-02-25 at 14.38.15_871409e4.jpg' />
-      
-      </div>
-      <button> See More <IoIosArrowRoundForward size={49} /></button>
-     </div>
 
+      <div className="sec4">
+        <h3>Our Gallery</h3>
+
+        <div className="gallery">
+          <img src="\WhatsApp Image 2025-02-25 at 14.38.15_0e404587.jpg" />
+          <div className="imgBody">
+            <img src="\WhatsApp Image 2025-02-25 at 14.52.29_cfa199ef.jpg" />
+            <img src="\WhatsApp Image 2025-02-25 at 14.41.53_832e0478.jpg" />
+          </div>
+          <img src="\WhatsApp Image 2025-02-25 at 14.38.15_871409e4.jpg" />
+        </div>
+        <div className="seemorediv">
+          <button className="seemore">
+            See More <IoIosArrowRoundForward size={49} />
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
